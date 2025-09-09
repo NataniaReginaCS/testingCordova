8b45e8c6e677e8637c0468a41904e4633b39bec9
NataniaReginaCS <220711686@students.uajy.ac.id>
2025-09-09 13:13:31 +0700

    update

--- src/android/OSPlugin.java
+++ src/android/OSPlugin.java
@@ -1,30 +1,30 @@
-package com.example.osplugin;
-
-import android.widget.Toast;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+/**
+ * This class echoes a string called from JavaScript.
+ */
 public class OSPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("showToast".equals(action)) {
+        if (action.equals("coolMethod")) {
             String message = args.getString(0);
-            int duration = args.getInt(1);
-            showToast(message, duration);
-            callbackContext.success();
+            this.coolMethod(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void showToast(final String message, final int duration) {
-        cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(cordova.getActivity(), message, duration).show();
-            }
-        });
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
     }
 }
