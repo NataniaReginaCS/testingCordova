7f970e94a0ee22af56618fb9d366e40a92838d9c
NataniaReginaCS <220711686@students.uajy.ac.id>
2025-09-09 16:08:52 +0700

    Update toastyplugin.js

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -1,8 +1,5 @@
-// Empty constructor
 function ToastyPlugin() {}
 
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
 ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
@@ -10,7 +7,6 @@ ToastyPlugin.prototype.show = function(message, duration, successCallback, error
   cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
 }
 
-// Installation constructor that binds ToastyPlugin to window
 ToastyPlugin.install = function() {
   if (!window.plugins) {
     window.plugins = {};
